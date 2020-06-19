cb673eb4453b6a433ea9c0d1082ee14adc89c6d6
crazywolf132 <crazywolf132@gmail.com>
2020-06-19 14:16:21 +1000

    feat: Using localStorage on homescreen
    Using the localstorage to access the liked, disliked, and watchList lists…
    
    They will then be displayed on the screen…
    
    If the watchlist is empty, you will get a message telling you to start by adding some movies first.

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState, useContext } from 'react';
 import AppContext from '../../context/AppContext';
 import { Navbar, Movie, List } from '../../components';
+import useLocalStorage from '../../handlers/useLocalStorage';
 
 export default function HomeScreen(props) {
 	const { dispatch } = useContext(AppContext);
+	const [watchList, setWatchList] = useLocalStorage('watchList', []);
+	const [likes, setLikes] = useLocalStorage('likes', []);
+	const [dislikes, setDislikes] = useLocalStorage('dislikes', []);
 	return (
 		<>
 			<Navbar
@@ -21,6 +25,19 @@ export default function HomeScreen(props) {
 					},
 				]}
 			/>
+			{watchList.length === 0 && (
+				<h1
+					style={{
+						marginTop: '10%',
+						fontSize: 36,
+						fontWeight: 600,
+						textAlign: 'center',
+						justifyContent: 'center',
+						alignItems: 'Center',
+					}}>
+					Lets start by adding some movies!
+				</h1>
+			)}
 		</>
 	);
 }
