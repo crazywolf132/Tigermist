f37588ccdb963a7115b134835cdd2c46f2f20f5f
crazywolf132 <crazywolf132@gmail.com>
2020-06-19 14:17:49 +1000

    fix: Added watchlist to homepage

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -3,12 +3,34 @@ import AppContext from '../../context/AppContext';
 import { Navbar, Movie, List } from '../../components';
 import { Title } from './style';
 import useLocalStorage from '../../handlers/useLocalStorage';
+import _ from 'lodash';
 
 export default function HomeScreen(props) {
 	const { dispatch } = useContext(AppContext);
+
 	const [watchList, setWatchList] = useLocalStorage('watchList', []);
 	const [likes, setLikes] = useLocalStorage('likes', []);
 	const [dislikes, setDislikes] = useLocalStorage('dislikes', []);
+
+	const changeLikeDislikeStatus = (movie, liked = true) => {
+		if (!liked) {
+			// Going to remove it from the liked list... if it is there.
+			setLikes(likes.filter((likedMovie) => likedMovie.ID != movie.ID));
+			setDislikes(_.uniq([...dislikes, movie]));
+		} else {
+			setDislikes(
+				dislikes.filter((dislikedMovie) => dislikedMovie.ID != movie.ID)
+			);
+			setLikes(_.uniq([...likes, movie]));
+		}
+	};
+
+	const removeFromWatchList = (movie) => {
+		setLikes(likes.filter((m) => m.ID !== movie.ID));
+		setDislikes(dislikes.filter((m) => m.ID !== movie.ID));
+		setWatchList(watchList.filter((m) => m.ID !== movie.ID));
+	};
+
 	return (
 		<>
 			<Navbar
@@ -29,6 +51,37 @@ export default function HomeScreen(props) {
 			{watchList.length >= 1 && (
 				<>
 					<Title style={{ marginLeft: '44px' }}>Watch List</Title>
+					<List style={{ marginLeft: '22px' }}>
+						{watchList.map((movie, index) => (
+							<Movie
+								key={index}
+								img={movie.Poster || ''}
+								title={movie.Title}
+								genre={movie.Genre}
+								time={movie.Runtime}
+								hoverButtons={[
+									{
+										title: '👍',
+										callBack: () =>
+											changeLikeDislikeStatus(movie),
+									},
+									{
+										title: '🗑️',
+										callBack: () =>
+											removeFromWatchList(movie),
+									},
+									{
+										title: '👎',
+										callBack: () =>
+											changeLikeDislikeStatus(
+												movie,
+												false
+											),
+									},
+								]}
+							/>
+						))}
+					</List>
 					{likes.length >= 1 && (
 						<Title style={{ marginLeft: '44px' }}>Liked</Title>
 					)}
